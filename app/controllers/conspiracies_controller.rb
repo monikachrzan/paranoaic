f53add82b50c2fdf6beae755c661aafee7a3a5b0
Mandil Khadka <khadkamandil62@gmail.com>
2025-08-19 16:26:55 +0900

    Small refactor-- Create & New function done

--- app/controllers/conspiracies_controller.rb
+++ app/controllers/conspiracies_controller.rb
@@ -8,20 +8,19 @@ class ConspiraciesController < ApplicationController
   end
 
   def create
-    @conspiracy = Conspiracy.new(create_params)
+    @conspiracy = Conspiracy.new(create_strong_params)
     @user = current_user
     @conspiracy.user = @user
     if @conspiracy.save
       redirect_to conspiracy_path(@conspiracy)
     else
-      raise
       render "new", status: :unprocessable_entity
     end
   end
 
   private
-  
-  def create_params
+
+  def create_strong_params
     params.require(:conspiracy).permit(:title, :content)
   end
 end
