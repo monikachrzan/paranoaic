55c00873f69274a5dde047308a5ef1c9c8c57d4d
stefan muhsin <stefanmuhsin@gmail.com>
2025-08-19 14:28:31 +0900

    initial push

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,15 @@
 class VotesController < ApplicationController
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = Vote.new
+    @conspiracy = Conspiracy.find(params[:id])
+    @user = current_user
+    @vote.conspiracy = @conspiracy
+    @vote.user = @user
+    if vote.save
+      if
+  end
 end
