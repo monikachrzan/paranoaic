3a0f2d15e00e1d8819c0216470800297cc65a907
cdGaetan Duquesnoy <gaetan.duquesnoy@gmail.com>
2025-08-19 15:16:36 +0900

    basic conspiracy index and show page done

--- app/controllers/conspiracies_controller.rb
+++ app/controllers/conspiracies_controller.rb
@@ -1,6 +1,7 @@
 class ConspiraciesController < ApplicationController
 
   def show
-    @conspiracy = Conspiracy.find(params[:id])
+    @user = User.find(params[:user_id])
+    @conspiracy = @user.conspiracies.find(params[:id])
   end
 end
